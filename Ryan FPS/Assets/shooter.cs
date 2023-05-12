dbba25822bdea056b433f7a01fcc132ef5d3fb4d
rokers <rokers@gmail.com>
2023-05-12 22:14:54 +0400

    ryan fps

---
+++ Ryan FPS/Assets/shooter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    public GameObject decalPrefab;
+
+    GameObject[] totalDecals;
+    int actual_decal = 0;
+
+    void Start()
+    {
+
+        totalDecals = new GameObject[10];
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(
+
+                Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)), out hit))
+            {
+                Destroy(totalDecals[actual_decal]);
+              totalDecals[actual_decal] =
+                 GameObject.Instantiate(decalPrefab, hit.point + hit.normal * 0.01f,
+                 Quaternion.FromToRotation(
+                   Vector3.forward, -hit.normal))
+
+               as GameObject;
+
+                actual_decal++;
+
+                if (actual_decal == 10) actual_decal = 0;
+            }
+        }
+    }
+}
